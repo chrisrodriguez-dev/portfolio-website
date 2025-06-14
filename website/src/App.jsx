f884dd9976dc6acc158f012c9c4ed6ab70fad878
chrisrodriguez-dev <christophernjrodriguez@gmail.com>
2025-06-14 11:17:22 -0400

    Added personal features

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -5,9 +5,28 @@ function App() {
 
   return (
     <>
-       
+       <div>
+        <p>This will be my portfolio website</p>
+        <p>It is currently a work in progress</p>
+        <Text display = "My name is Christopher M. Rodriguez"/>
+        <a 
+        href="https://www.linkedin.com/in/chris-rodriguez0" 
+        target="_blank"
+        className="inline-block px-6 py-2 bg-blue-600 text-black font-semibold rounded hover:bg-black hover:text-white transition"
+        >
+          Visist my LinkedIn
+        </a>
+       </div>
     </>
   )
+}
+
+function Text({display}){
+  return (
+    <div>
+      <p>{display}</p>
+    </div>
+  );
 }
 
 export default App
